2218a7749db354d98469eaca3d90076dc66f2882
DatoGari <dgaribashvili@gmail.com>
2025-05-25 13:15:35 +0400

    add ingredients

---
+++ src/pages/IngredientsPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import IngredientsTable from "../components/tables/IngredientsTable";
+
+const IngredientsPage = () => {
+  return <IngredientsTable />;
+};
+
+export default IngredientsPage;
